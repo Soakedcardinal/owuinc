b30b10d5153afd787f01a56cd0d2a8cd5aa9adf0
Soakedcardinal <github@soaraza.com>
2026-09-13 14:53:34 -0700

    version: 3.8.0

--- owuinc/owuinc.py
+++ owuinc/owuinc.py
@@ -4,7 +4,7 @@ author: soakedcardinal
 git_url: https://github.com/soakedcardinal/owuinc
 description: Manage files, tasks, and calendars via WebDAV and CalDAV.
 requirements: caldav>=3.0.0,icalendar,aiowebdav2
-version: 3.7.3
+version: 3.8.0
 license: MIT
 """
 
